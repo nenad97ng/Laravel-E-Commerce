e004bf1829aedb83caba3b15d9216911f83dd5ba
nenad97ng <nenad97ng@gmail.com>
2021-10-17 10:48:33 +0200

    Create Product view  (Update Bootstrap version)
    Product view connect with DB. Created carousel slide.
    Bootstrap updated to version 5.1.3

--- eComm-en/resources/views/header.blade.php
+++ eComm-en/resources/views/header.blade.php
@@ -1,26 +1,27 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">Brand</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-  
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Orders</a>
+          <a class="nav-link active" aria-current="page" href="#">Orders</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-          <li class="nav-item">
-        <a class="nav-link" href="#">Cart(0)</a>
-          </li>
-      </ul>
+        <li class="nav-item">
+      <a class="nav-link" href="#">Cart(0)</a>
+        </li>
+    </ul>
     </div>
-  </nav>
+  </div>
+</nav>
--- eComm-en/resources/views/login.blade.php
+++ eComm-en/resources/views/login.blade.php
@@ -5,18 +5,18 @@
         <div class="col-sm-4 ">
 
             <form action="/login" method="POST">
-                <div class="form-group">
-                    @csrf
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                @csrf
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
-            </form>
+              </form>
             
         </div>    
     </div>
