d257498519868fbc1f8f98850bbd351bbb82d2d1
nenad97ng <nenad97ng@gmail.com>
2021-11-01 15:55:11 +0100

    Update CSS File

--- eComm-en/resources/views/master.blade.php
+++ eComm-en/resources/views/master.blade.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/main.css') }}" />
+    
     <title>E-commerce Project</title>
 </head>
 <body>
@@ -19,42 +24,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
-<style>
-    .custom-login{
-        height: 500px;
-        padding-top: 300px;
-    }
-    img.slider-img{
-        height: 400px !important;
-    }
-    .custom-product{
-        height: 600px;
-    }
-    .slider-text{
-        background-color: rgba(117, 117, 117, 0.40) !important;
-    }
-    .trending-image{
-        height: 100px;
-    }
-    .trending-item{
-        float: left;
-        width: 20%;
-    }
-    .trending-wrapper{
-        margin: 30px;
-    }
-    .detail-img{
-        height: 200px;
-    }
-    .search-box{
-        width: 500px !important;
-    }
-    .footer-bottom{
-        position: relative;
-        top: 200px;
-        bottom: 0;
-        height: 100px;
-    }
-</style>
 
 </html>
